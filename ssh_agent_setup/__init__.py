267da3642498a798fb488d5c9e930bb2fdc23801
Yoav Kleinberger <haarcuba@gmail.com>
2021-09-27 20:32:36 +0300

    conventions

--- ssh_agent_setup/__init__.py
+++ ssh_agent_setup/__init__.py
@@ -5,40 +5,40 @@ import re
 import atexit
 
 
-def _killAgent():
+def _kill_agent():
     logging.info('killing previously started ssh-agent')
     subprocess.run(['ssh-agent', '-k'])
     del os.environ['SSH_AUTH_SOCK']
     del os.environ['SSH_AGENT_PID']
 
 
-def _setupAgent():
+def _setup_agent():
     process = subprocess.run(['ssh-agent', '-s'], stdout=subprocess.PIPE, text=True)
     OUTPUT_PATTERN = re.compile('SSH_AUTH_SOCK=(?P<socket>[^;]+).*SSH_AGENT_PID=(?P<pid>\d+)', re.MULTILINE | re.DOTALL)
     match = OUTPUT_PATTERN.search(process.stdout)
     if match is None:
         raise Exception('Could not parse ssh-agent output. It was: {}'.format(process.stdout))
-    agentData = match.groupdict()
-    logging.info('ssh agent data: {}'.format(agentData))
+    agent_data = match.groupdict()
+    logging.info('ssh agent data: {}'.format(agent_data))
     logging.info('exporting ssh agent environment variables')
-    os.environ['SSH_AUTH_SOCK'] = agentData['socket']
-    os.environ['SSH_AGENT_PID'] = agentData['pid']
-    atexit.register(_killAgent)
+    os.environ['SSH_AUTH_SOCK'] = agent_data['socket']
+    os.environ['SSH_AGENT_PID'] = agent_data['pid']
+    atexit.register(_kill_agent)
 
 
-def agentUp():
+def agent_up():
     return os.environ.get('SSH_AUTH_SOCK') is not None
 
 
 def setup():
-    if agentUp():
+    if agent_up():
         logging.info('ssh-agent already present')
     else:
         logging.info('ssh-agent not present, will now set it up')
-        _setupAgent()
+        _setup_agent()
 
 
-def addKey(key_file):
+def add_key(key_file):
     process = subprocess.run(['ssh-add', key_file])
     if process.returncode != 0:
         raise Exception('failed to add the key: {}'.format(key_file))
